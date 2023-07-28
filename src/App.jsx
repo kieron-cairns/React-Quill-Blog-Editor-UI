f402543cc8f058f1468345d9e120c3049d0f8dc7
Kieron Cairns <kieroncairns@protonmail.com>
2023-07-28 20:07:11 +0100

    CHANGE: Blog html partly implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
+import ReactDOMServer from 'react-dom/server';
+
 import 'react-quill/dist/quill.snow.css';
 
 function MyComponent() {
-  const [value, setValue] = useState('');
+
   const quillRef = useRef(); // Create a ref for storing the Quill instance
+
+   // Define your initial blog posts data
+   const [blogPosts, setBlogPosts] = useState([
+    {
+      link: "/blog/business-consulting",
+      img: "./logo512.png",
+      alt: "business consulting",
+      title: "Business Consulting",
+      desc: "Get expert advice to help your business grow and succeed.",
+      date: "July 13, 2023",
+      content: <div class="code-block"><pre><code class="language-javascript">{`public void HeyWorld() {
+        Console.WriteLine("Hello World");
+      }
+      `}</code></pre></div>
+    },
+    // other posts...
+  ]);
+
+  const [value, setValue] = useState(''); // Initialize Quill's value to an empty string
+
+// Function to handle selecting a blog post
+const handleSelectPost = (index) => {
+  let content = blogPosts[index].content;
+
+  if (typeof content !== 'string') {
+    content = ReactDOMServer.renderToStaticMarkup(content);
+  }
+  
+  // Remove the special characters `{` and `}` around the backticks
+  content = content.replace(/<code[^>]*>{`/g, '<code>');
+  content = content.replace(/`}<\/code>/g, '</code>');
+
+  setValue(content); // Load the HTML content of the selected post into Quill
+};
+
 
   useEffect(() => {
     if (quillRef != null && quillRef.current != null) {
@@ -53,6 +90,8 @@ function MyComponent() {
         onChange={setValue} 
         modules={modules} 
       />
+            <button onClick={() => handleSelectPost(0)}>Load post</button> {/* Load the content of the first post into Quill */}
+
       <button onClick={handleExport}>Export as HTML</button> {/* Add export button */}
 
      
